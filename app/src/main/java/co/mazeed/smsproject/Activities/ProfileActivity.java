23f22d77b5d52fe31ebbe3be0f550ee8456c2799
amorenew <amorenew@gmail.com>
2015-05-29 01:03:59 +0200

    add icon and splash images

--- app/src/main/java/co/mazeed/smsproject/Activities/ProfileActivity.java
+++ app/src/main/java/co/mazeed/smsproject/Activities/ProfileActivity.java
@@ -2,8 +2,6 @@ package co.mazeed.smsproject.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import co.mazeed.smsproject.R;
 
--- app/src/main/java/co/mazeed/smsproject/Activities/SplashActivity.java
+++ app/src/main/java/co/mazeed/smsproject/Activities/SplashActivity.java
@@ -3,10 +3,6 @@ package co.mazeed.smsproject.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import co.mazeed.smsproject.R;
 
