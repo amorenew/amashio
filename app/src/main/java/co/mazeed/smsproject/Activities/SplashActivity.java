f0e101bcbadc2a2ab3e46513923662d4aace69c8
amorenew <amorenew@gmail.com>
2015-06-07 00:33:24 +0200

    my account profile styles

--- app/src/main/java/co/mazeed/smsproject/Activities/SplashActivity.java
+++ app/src/main/java/co/mazeed/smsproject/Activities/SplashActivity.java
@@ -29,6 +29,16 @@ public class SplashActivity extends Activity {
         startActivity(intent2);
         Intent intent3 = new Intent(this, SignUpActivity.class);
         startActivity(intent3);
+        Intent intent4 = new Intent(this, ProfileActivity.class);
+        startActivity(intent4);
+//        Intent intent5 = new Intent(this, SignUpActivity.class);
+//        startActivity(intent5);
+//        Intent intent6 = new Intent(this, SignUpActivity.class);
+//        startActivity(intent6);
+//        Intent intent7= new Intent(this, SignUpActivity.class);
+//        startActivity(intent7);
+//        Intent intent8= new Intent(this, SignUpActivity.class);
+//        startActivity(intent8);
     }
 
 }
