0717678ebcd1a7aa2e5bbcc763be4b4a34f9b1e4
amorenew <amorenew@gmail.com>
2015-06-06 22:13:44 +0200

    guide screen added language to tool bar

--- app/src/main/java/co/mazeed/smsproject/Activities/GuideActivity.java
+++ app/src/main/java/co/mazeed/smsproject/Activities/GuideActivity.java
@@ -3,6 +3,8 @@ package co.mazeed.smsproject.Activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import co.mazeed.smsproject.R;
 
@@ -21,5 +23,29 @@ public class GuideActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_guide, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_language) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
